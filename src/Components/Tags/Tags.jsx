4cf71300673bde48b8080c3ddc5de832d5bc85d3
eduardovk <eduardo_vk@hotmail.com>
2021-07-22 18:11:15 -0300

    Adicionado react-icons e icones de share e report no tag container

--- src/Components/Tags/Tags.jsx
+++ src/Components/Tags/Tags.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import "./style.css";
+import { FiAlertTriangle } from 'react-icons/fi'
+import { GiShare } from 'react-icons/gi'
 
 function Tags() {
     return (
         <div className="tags-container">
             <h3>Tags: </h3>
-            <ul className="genre-tags">
-                <li>indie rock</li>
-                <li>pop rock</li>
-                <li>heavy metal</li>
-            </ul>
+            <span>indie rock</span>
+            <span>pop rock</span>
+            <span>heavy metal</span>
+            <div className="share-report">
+                <GiShare size="2em" />
+                <FiAlertTriangle size="1.8em" />
+            </div>
         </div>
     );
 }
