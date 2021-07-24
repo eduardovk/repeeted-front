ece4026d726eeee675170ce0abadc230cc9bb37a
eduardovk <eduardo_vk@hotmail.com>
2021-07-24 11:27:20 -0300

    Criado componente Navbar (wireframe)

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import WordCloud from './Components/WordCloud';
 import Tags from './Components/Tags';
 import Accordion from './Components/Accordion';
 import ArtistCards from './Components/ArtistCards';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <ArtistInfo />
-        <WordCloud />
-        <Tags />
-        <Accordion />
-        <ArtistCards />
+      <NavBar />
+      <div className="main">
+        <div className="container">
+          <ArtistInfo />
+          <WordCloud />
+          <Tags />
+          <Accordion />
+          <ArtistCards />
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import "./style.css";
+import { GiHamburgerMenu } from 'react-icons/gi'
+
+function NavBar() {
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    return (
+        <nav>
+            <div className="navbar">
+                <div className="navbar-logo">Logo</div>
+                <ul className="navbar-list">
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Como Funciona</li>
+                    <li>Doar</li>
+                </ul>
+                <div className="menu-icon">
+                    <span onClick={()=>setShowMenu(!showMenu)}><GiHamburgerMenu size="1.8em" /></span>
+                </div>
+                <div className="mobile-nav" style={showMenu ? ({ display: "flex" }) : ({ display: "none" })}>
+                    <span>Home</span>
+                    <span>Sobre</span>
+                    <span>Como Funciona</span>
+                    <span>Doar</span>
+                </div>
+            </div>
+        </nav>
+    );
+
+}
+export default NavBar;
