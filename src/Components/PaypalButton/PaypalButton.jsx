f49f4a37ca08f08e0cd9509a4644c07c2b775b9a
eduardovk <eduardo_vk@hotmail.com>
2021-09-19 12:42:55 -0300

    Adicionado tracking de click no botao do paypal

--- src/Components/PaypalButton/PaypalButton.jsx
+++ src/Components/PaypalButton/PaypalButton.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { i18n } from '../../translate/i18n';
 import { GlobalContext } from '../../Contexts/GlobalContext';
+import ReactGa from 'react-ga';
 
 function PaypalButton() {
 
@@ -11,12 +12,21 @@ function PaypalButton() {
         setCurrency(lang === 'pt-BR' ? 'BRL' : 'USD');
     }, [currency, lang]);
 
+    const handleClick = () => {
+        ReactGa.event({
+            category: 'Button',
+            action: 'Paypal button click'
+        });
+    }
+
     return (
         <form action="https://www.paypal.com/donate" method="post" target="_top">
             <input type="hidden" name="business" value="WYLDPSDW4YBBU" />
             <input type="hidden" name="no_recurring" value="0" />
             <input type="hidden" name="currency_code" value={currency} />
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title={i18n.t('buttons.paypalTitle')} alt={i18n.t('buttons.paypalAlt')} />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0"
+                onClick={handleClick}
+                name="submit" title={i18n.t('buttons.paypalTitle')} alt={i18n.t('buttons.paypalAlt')} />
         </form>
     );
 }
